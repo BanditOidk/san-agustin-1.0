7e07d024d33e009208044e00b4bca14b0e22f00c
BanditOidk <112146295+BanditOidk@users.noreply.github.com>
2022-10-17 09:59:35 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,7 @@
 <?php
-$conexion=pg_connect("host=localhost dbname=bdcatalogo user=postgres password=123456");
+$conn_string = "ec2-54-160-200-167.compute-1.amazonaws.com
+port=5432 dbname=dbtuvjl2a221e8 user=mkdbsogdwalntr password=bd505265106944247031247f08852a5febb81a2a0650565aeeacc70bc4e7e693
+";
+$conexion = pg_connect($conn_string);
 
 ?>
-
