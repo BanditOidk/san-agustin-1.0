6bb5f56162971fb6a514d40f48c6765d267123b8
BanditOidk <112146295+BanditOidk@users.noreply.github.com>
2022-10-17 10:03:32 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conn_string = "ec2-54-160-200-167.compute-1.amazonaws.com
+$conn_string = "host=ec2-54-160-200-167.compute-1.amazonaws.com
 port=5432 dbname=dbtuvjl2a221e8 user=mkdbsogdwalntr password=bd505265106944247031247f08852a5febb81a2a0650565aeeacc70bc4e7e693
 ";
 $conexion = pg_connect($conn_string);
