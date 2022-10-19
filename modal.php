fbfe6b56a84d02cb1cdd75529f41ad1842717562
BanditOidk <caroxd100@gmail.com>
2022-10-19 12:36:58 -0500

    dvfcsdf

--- modal.php
+++ modal.php
@@ -12,8 +12,13 @@ $data = pg_fetch_assoc($consulta);
  <html>
  
  <head>
+ <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="stylesheet" href="css/estilos.css?<?=date('Y-m-d H:i:s')?>">
+    <link rel="icon" type="image/x-icon" href="images/icon.png">
  	<title>datos de producto</title>
-	 <link rel="stylesheet" href="css/estilos.css?<?=date('Y-m-d H:i:s')?>">
+	 
  </head>
  <body>
  <?php include "template/header.php"; ?>
@@ -23,21 +28,21 @@ $data = pg_fetch_assoc($consulta);
 
       <h1><?php echo $data['nombre'];?></h1>
  	<img src="<?php echo ($data['imagen']); ?>">
-    <p ><?php echo $data['descripcion1']; ?></p>
-    <p ><?php echo $data['descripcion2']; ?></p>
-    <p ><?php echo $data['descripcion3']; ?></p>
-    <p ><?php echo $data['descripcion4']; ?></p>
-    <p ><?php echo $data['descripcion5']; ?></p>
-    <p ><?php echo $data['descripcion6']; ?></p>
-    <p ><?php echo $data['descripcion7']; ?></p>
-    <p ><?php echo $data['descripcion8']; ?></p>
-	<h2>s/.<?php echo $data ['precio']; ?></h2>
+     <p ><?php echo $data['descripcion1']; ?></p>
+     <p ><?php echo $data['descripcion2']; ?></p>
+     <p ><?php echo $data['descripcion3']; ?></p>
+     <p ><?php echo $data['descripcion4']; ?></p>
+     <p ><?php echo $data['descripcion5']; ?></p>
+     <p ><?php echo $data['descripcion6']; ?></p>
+     <p ><?php echo $data['descripcion7']; ?></p>
+     <p ><?php echo $data['descripcion8']; ?></p>
+ 	<h2>s/.<?php echo $data ['precio']; ?></h2>
 		 
 
 		 </section>
-		 </div>
+           </div>
+           
  </body>
- <br><br><br><br>
  <?php include "template/footer.php"; ?>
-
  </html>
+ 
