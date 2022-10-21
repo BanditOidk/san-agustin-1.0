71790724013dd2c69467a361b26fd56bd4d90038
BanditOidk <caroxd100@gmail.com>
2022-10-21 12:53:19 -0500

    sdvsdfg

---
+++ soporte.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css?<?=date('Y-m-d H:i:s')?>">
+    <link rel="icon" type="image/x-icon" href="images/icon.png">
+    <title>Soporte</title>
+</head>
+<body>
+<?php include "template/header.php"; ?>
+<br><br><br><br><br>
+<div class="card">
+    <h1>SOPORTE TECNICO</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores maxime eos rerum mollitia cupiditate ab similique quod nisi ipsa exercitationem maiores laborum dignissimos aliquam possimus accusamus, quaerat quisquam perspiciatis aliquid.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, veniam! Voluptas, neque necessitatibus eveniet tempora at molestias deleniti doloribus ad deserunt mollitia. Officiis illo natus eos, harum consequuntur incidunt eius.
+    </p>
+    <img src="images/productos/audifonos.jpg" alt="" width="250" height="300">
+</div>
+<?php include "template/footer.php"; ?>
+</body>
+
+</html>
